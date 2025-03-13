93eea45f6dc612660293800ea712ffd43432e95b
Sonny Ding <sonnyding20@gmail.com>
2025-03-13 15:34:52 -0700

    Add security state variables for client-server handshake process

--- project/security.c
+++ project/security.c
@@ -6,14 +6,24 @@
 #include <string.h>
 #include <unistd.h>
 
+bool is_client = false;
+bool is_handshake_done = false;
+
+bool is_client_hello_sent = false;
+bool is_server_hello_received = false;
+bool is_client_hello_received = false;
+bool is_finished_receive = false;
+
 void init_sec(int type, char* host) {
     init_io();
 }
 
 ssize_t input_sec(uint8_t* buf, size_t max_length) {
+    
     return input_io(buf, max_length);
 }
 
 void output_sec(uint8_t* buf, size_t length) {
     output_io(buf, length);
 }
+
